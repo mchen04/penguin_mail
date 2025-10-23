7eb51e38ce262629ee5d225aa30b5ab97c1fe20f
MarkGhebrial <mghebrialjr@icloud.com>
2025-10-23 11:46:20 -0700

    Initialize django project

---
+++ backend/penguin_mail/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for penguin_mail project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'penguin_mail.settings')
+
+application = get_asgi_application()
