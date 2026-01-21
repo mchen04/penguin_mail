6df5346ed7e93d32bf3657663bc01de1e6ba15d9
Sped32DJ <dannycool10236@gmail.com>
2026-01-20 18:38:36 -0800

    Adding some notes

---
+++ backend/compose.py
@@ -0,0 +1,14 @@
+# Import libraries
+
+# Compose should output into a json format?
+# Draft and sent emails should be stored in a database and stored locally in a json format
+
+# grab all this data from frontend compose feature (via and HTTP post request)
+
+# Choose email account (source of email), grab credentials from credentials manager (credentials.py)
+
+# To[], CC[], BCC[]
+
+# subject, body
+
+# Then actually send the email via SMTP server
